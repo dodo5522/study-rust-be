c77e9a389254b71f23e0c6a0b514656cd47182e8
Takashi Ando <dodo5522@gmail.com>
2025-10-26 22:08:33 +0900

    [presentation] Support .env file

--- src/presentation/main.rs
+++ src/presentation/main.rs
@@ -1,12 +1,17 @@
+use dotenvy::var;
+
 use super::get_router;
 
 /// Run the application server.
 pub async fn run() -> anyhow::Result<()> {
+    let bind_addr = var("BIND_ADDR")?;
+    let bind_port = var("BIND_PORT")?;
+
     tracing_subscriber::fmt::init();
 
     // run our app with hyper, listening globally on the port
     let router = get_router();
-    let listener = tokio::net::TcpListener::bind("0.0.0.0:3000").await?;
+    let listener = tokio::net::TcpListener::bind(format!("{bind_addr}:{bind_port}")).await?;
     axum::serve(listener, router).await?;
 
     Ok(())
