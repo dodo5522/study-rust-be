a66fab7a24aa137f80a307848ed84b7604a78dac
Takashi Ando <dodo5522@gmail.com>
2025-12-21 15:03:10 +0900

    [infra] Use smol instead of async-std

--- layers/infra-db-migration/src/main.rs
+++ layers/infra-db-migration/src/main.rs
@@ -3,10 +3,11 @@ mod env;
 use anyhow::Result;
 use sea_orm_migration::prelude::*;
 use std::env::set_var;
+use async_compat::Compat;
 
-#[async_std::main]
-async fn main() -> Result<()> {
+fn main() -> Result<()> {
     set_var("DATABASE_URL", env::get_db_url()?);
-    cli::run_cli(migration::Migrator).await;
-    Ok(())
+    Ok(smol::block_on(Compat::new(async {
+        cli::run_cli(migration::Migrator).await;
+    })))
 }
